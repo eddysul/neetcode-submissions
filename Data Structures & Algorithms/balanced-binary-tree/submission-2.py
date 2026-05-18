12136bd3de5d637466a4b74153e0f283b002e484
Eddy (Min Gyu) Sul <eddysul1100@gmail.com>
2026-05-18 14:58:15 -0700

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        # Look at 1 node and its immediate children at each time
+        # We want to know eventually heights of left and right subtree
+        # So we are passing, returning heights UP
+        # When we hit leaf, we get left and right subtree and compare then if True push up
+        # At any point if left and right subtrees have different distance we return False
+        # Our helper function is for returning height of trees
+
+        # Main function variable should return the boolean of whether tree is balanced or not
+
+        def getHeight(node):
+            if not node:
+                return [True, 0]
+            
+            left = getHeight(node.left)
+            right = getHeight(node.right)
+
+            balanced = left[0] and right[0] and abs(left[1]-right[1]) <= 1
+
+            return [balanced, 1+max(left[1], right[1])]
+
+        return getHeight(root)[0]
+        
+        
